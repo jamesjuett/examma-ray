0ac4f51668a0ed8bffc3ceb4169980cbaa6f5faa
James Juett <jjuett@umich.edu>
2022-09-25 17:19:24 -0400

    add referrer policy

--- src/core/exam_renderer.ts
+++ src/core/exam_renderer.ts
@@ -73,6 +73,7 @@ export abstract class ExamRenderer {
       <html>
       <head>
         <meta charset="UTF-8">
+        <meta name="referrer" content="strict-origin-when-cross-origin" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://unpkg.com/@popperjs/core@2" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
